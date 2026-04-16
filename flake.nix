66978e61e7a6eaf0a228fef030ad59b9996ba196
uonr <dev@yuru.me>
2026-04-16 20:23:56 +0900

    Initial

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "My Devshells";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        comfyui =
+          let
+            nixLibPath = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc.lib
+              pkgs.zlib
+              pkgs.libglvnd
+              pkgs.glib
+            ];
+            runtimeLibPath = "${nixLibPath}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+          in
+          pkgs.mkShell {
+            packages = [
+              pkgs.python313
+            ];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH="${runtimeLibPath}:''${LD_LIBRARY_PATH:-}"
+            '';
+          };
+      });
+    };
+}
