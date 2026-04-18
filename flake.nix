690494b5a64274b6ea318478507e2b8a21025622
uonr <dev@yuru.me>
2026-04-19 03:49:24 +0900

    Update comfyui devshell

--- flake.nix
+++ flake.nix
@@ -25,16 +25,23 @@
               pkgs.libglvnd
               pkgs.glib
               pkgs.iconv
+              pkgs.libxcb
             ];
             runtimeLibPath = "${nixLibPath}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
           in
           pkgs.mkShell {
             packages = [
               pkgs.python313
+              pkgs.uv
+              pkgs.python313Packages.huggingface-hub
+              pkgs.ffmpeg
+              pkgs.git-lfs
+              pkgs.ruff
             ];
 
             shellHook = ''
               export LD_LIBRARY_PATH="${runtimeLibPath}:''${LD_LIBRARY_PATH:-}"
+              export TRITON_LIBCUDA_PATH=/run/opengl-driver/lib
             '';
           };
       });
